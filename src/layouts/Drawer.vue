6786dbcec91ed1102a3d7e3d86ddac8063ffacb3
Jose Garcia <jogarcia@tuta.io>
2023-02-26 09:38:15 -0500

    Remove unnecessary directive

--- src/layouts/Drawer.vue
+++ src/layouts/Drawer.vue
@@ -4,9 +4,7 @@
       <v-list-item-avatar color="white">
         <v-img :src="logo" height="34" contain />
       </v-list-item-avatar>
-      <v-list-item-title class="title">
-        SGE
-      </v-list-item-title>
+      <v-list-item-title class="title"> SGE </v-list-item-title>
     </v-list-item>
     <v-list-item>
       <v-list-item-subtitle class="institution-name-styles">
@@ -17,12 +15,17 @@
     <v-divider />
 
     <v-list nav :color="color">
-      <v-list-item v-for="(link, i) in links" :key="i" class="mt-3" :to="link.to">
+      <v-list-item
+        v-for="(link, i) in links"
+        :key="i"
+        class="mt-3"
+        :to="link.to"
+      >
         <v-list-item-icon>
           <v-icon>{{ link.icon }}</v-icon>
         </v-list-item-icon>
         <v-list-item-content>
-          <v-list-item-title v-text="$t(link.text)" />
+          <v-list-item-title>{{ $t(link.text) }} </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
     </v-list>
@@ -31,50 +34,49 @@
 
 <script>
 // Utilities
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from "vuex";
 
 export default {
   props: {
     opened: {
       type: Boolean,
-      default: false
+      default: false,
     },
     links: {
       type: Array,
-      required: true
-    }
+      required: true,
+    },
   },
   data: () => ({
-    logo: '/logo.png',
+    logo: "/logo.png",
     navDrawer: false,
-    mini: false
+    mini: false,
   }),
   computed: {
-    ...mapState('app', ['color', 'drawer', 'subtitle'])
+    ...mapState("app", ["color", "drawer", "subtitle"]),
   },
   watch: {
-    async navDrawer (val) {
-      await this.setDrawer({ drawer: val })
-    }
+    async navDrawer(val) {
+      await this.setDrawer({ drawer: val });
+    },
   },
-  mounted () {
-    this.navDrawer = this.drawer === 'true' || this.drawer === true
-    this.$root.$on('drawer_btn_clicked', () => {
-      this.navDrawer = !this.navDrawer
-    })
+  mounted() {
+    this.navDrawer = this.drawer === "true" || this.drawer === true;
+    this.$root.$on("drawer_btn_clicked", () => {
+      this.navDrawer = !this.navDrawer;
+    });
   },
   methods: {
-    ...mapActions('app', ['setDrawer'])
-  }
-
-}
+    ...mapActions("app", ["setDrawer"]),
+  },
+};
 </script>
 
 <style scoped>
 .institution-name-styles {
-    overflow: unset;
-    text-overflow: unset;
-    white-space: unset;
-    color: white !important;
+  overflow: unset;
+  text-overflow: unset;
+  white-space: unset;
+  color: white !important;
 }
 </style>
