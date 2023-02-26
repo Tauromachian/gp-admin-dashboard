eb7509f002d84e988caa23030f5b806318b81d01
Jose Garcia <jogarcia@tuta.io>
2023-02-26 17:10:59 -0500

    Load all components from the generic folder automatically

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -9,8 +9,10 @@ import { loadFonts } from "./webfontloader";
 import vuetify from "./vuetify";
 import pinia from "../stores";
 import router from "../router";
+import loadComponents from "./componentsLoader";
 
 export function registerPlugins(app) {
   loadFonts();
+  loadComponents(app);
   app.use(vuetify).use(pinia).use(router);
 }
