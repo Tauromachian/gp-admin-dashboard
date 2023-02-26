9030a096362f179c200c8ec6a016cec928021b50
Jose Garcia <jogarcia@tuta.io>
2023-02-26 11:41:09 -0500

    Make basic rules field

---
+++ src/utils/rules.js
@@ -0,0 +1,17 @@
+import { isNumber } from "./regex";
+
+export function required(message = "Este valor es requerido.") {
+  return (v) => !!v || message;
+}
+
+export function number(message = "Este campo debe ser un número.") {
+  return (v) => isNumber(v) || message;
+}
+
+export function password() {
+  return (v) => {
+    if (!v) return "La contraseña es requerida";
+
+    if (v.length < 8) return "La contraseña debe tener 8 caracteres o más";
+  };
+}
