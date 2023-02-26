658443ee281561fb7db5abe09ec1e87f2ffbb23a
Jose Garcia <jogarcia@tuta.io>
2023-02-26 08:56:26 -0500

    Remove lang

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,26 +3,6 @@ import store from "~/store/index";
 
 const token = store.getters["auth/token"];
 
-axios.interceptors.request.use(
-  (config) => {
-    config.headers["X-Requested-With"] = "XMLHttpRequest";
-
-    if (token) {
-      config.headers.common["Authorization"] = `Bearer ${token}`;
-    }
-
-    const locale = store.getters["lang/locale"];
-    if (locale) {
-      config.headers.common["Accept-Language"] = locale;
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
 axios.interceptors.response.use(
   (response) => {
     return response;
