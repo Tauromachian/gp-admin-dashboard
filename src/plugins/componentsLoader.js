f861b12d33c8d1179e1ae32d494e652ef4dac327
Jose Garcia <jogarcia@tuta.io>
2023-02-26 21:25:18 -0500

    Fix/ Import correctly

--- src/plugins/componentsLoader.js
+++ src/plugins/componentsLoader.js
@@ -2,7 +2,7 @@ const modules = import.meta.glob("@/components/generic/*.vue", { eager: true });
 
 export default function loadComponents(app) {
   for (const path in modules) {
-    const componentName = path.split("/").at(-1);
-    app.component(`Gen${componentName}`, modules[path]);
+    const componentName = path.split("/").at(-1).split(".")[0];
+    app.component(`Gen${componentName}`, modules[path].default);
   }
 }
