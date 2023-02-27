d6b799f7533f5bf08de7a7e61cba54d6b2d2a8f9
Jose Garcia <jogarcia@tuta.io>
2023-02-27 10:55:32 -0500

    Make sure routes always have backslash

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,10 @@
-export const HOST_DIR = import.meta.env.VITE_HOST_DIR;
-export const AUTH_DIR = import.meta.env.VITE_AUTH_DIR;
+let hostDir = import.meta.env.VITE_HOST_DIR;
+let authDir = import.meta.env.VITE_AUTH_DIR;
+
+export const HOST_DIR = appendSlash(hostDir);
+export const AUTH_DIR = appendSlash(authDir);
 export const API_DIR = `${HOST_DIR}`;
+
+function appendSlash(url) {
+  return url[url.length - 1] === "/" ? url : url + "/";
+}
