01ce98b5f52f37f4922863fbe05fa5085c2e3f45
Jose Garcia <jogarcia@tuta.io>
2023-02-27 21:20:56 -0500

    Create NotificationsHandler component

---
+++ src/components/generic/NotificationsHandler.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="w-100 mt-10 notifications-handler">
+    <gen-notification class="notification"></gen-notification>
+  </div>
+</template>
+
+<script>
+import GenNotification from "./Notification.vue";
+
+export default {
+  name: "NotificationsHandler",
+  components: {
+    GenNotification,
+  },
+
+  data() {
+    return {
+      messagesDirectory: {},
+    };
+  },
+
+  methods: {
+    deleteMessageAfterDelay(id) {
+      setTimeout(() => {
+        this.deleteMessage(id);
+      }, 5000);
+    },
+
+    deleteMessage(id) {
+      delete this.messagesDirectory[id];
+      this.messagesDirectory = { ...this.messagesDirectory };
+    },
+
+    addMessage(message) {
+      const id = getRandomString();
+
+      this.$set(this.messagesDirectory, id, message);
+
+      this.deleteMessageAfterDelay(id);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.notifications-handler {
+  position: fixed;
+  z-index: 1000;
+}
+
+.notification {
+  position: absolute;
+}
+</style>
