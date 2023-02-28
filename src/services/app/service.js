c40c35089d4a6dd41deb5b07b48a5464278b4c6a
Jose Garcia <jogarcia@tuta.io>
2023-02-28 17:28:49 -0500

    Make service view's services file

---
+++ src/services/app/service.js
@@ -0,0 +1,36 @@
+import qs from "query-string";
+
+import appApi from "../appApi";
+
+export async function getServices(filters) {
+  const stringifiedFilters = qs.stringify(filters);
+
+  const { data } = await appApi.get(`services?${stringifiedFilters}`);
+  return data;
+}
+
+export async function getService(id) {
+  const { data } = await appApi.get(`services/${id}`);
+  return data;
+}
+
+export async function addService(form) {
+  const { data } = await appApi.post(`services`, form);
+  return data;
+}
+
+export async function addServiceCredentials(id, form) {
+  const { data } = await appApi.post(`service/${id}/user`, form);
+  return data;
+}
+
+export async function updateService(id, form) {
+  const { data } = await appApi.patch(`services/${id}`, form);
+  return data;
+}
+
+export async function deleteService(id) {
+  const response = await appApi.delete(`service/${id}`);
+
+  return response.data.name;
+}
