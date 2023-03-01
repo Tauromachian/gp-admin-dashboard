c0e70377d152c09375ddac32d34d6b6bf7f73fa0
Jose Garcia <jogarcia@tuta.io>
2023-03-01 11:56:12 -0500

    Make basic DialogForm component

---
+++ src/components/generic/DialogForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-dialog v-model="dialog">
+    <gen-form v-bind="$attrs">
+      <slot></slot>
+    </gen-form>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "DialogForm",
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    dialog: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
