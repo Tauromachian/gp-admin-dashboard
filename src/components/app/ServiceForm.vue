b2940e5f47c526210ce18af475b6b2815f08f97a
Jose Garcia <jogarcia@tuta.io>
2023-02-28 19:48:33 -0500

    Ref/ Remove unnecessary code

--- src/components/app/ServiceForm.vue
+++ src/components/app/ServiceForm.vue
@@ -1,170 +1,160 @@
 <template>
-  <gen-form :title="$t('services.form_name')">
-    <v-form ref="form">
-      <v-container py-0>
-        <v-text-field
-          v-model="form.meter_no"
-          :label="$t('services.fields.meter_number')"
-          :rules="meterNumberRules"
-        />
-        <v-text-field
-          v-model="form.name"
-          :label="$t('services.fields.service_name')"
-          :rules="serviceNameRules"
-        />
+  <gen-form ref="form" :title="$t('services.form_name')">
+    <v-text-field
+      v-model="form.meter_no"
+      :label="$t('services.fields.meter_number')"
+      :rules="[rules.required(), rules.number()]"
+    />
+    <v-text-field
+      v-model="form.name"
+      :label="$t('services.fields.service_name')"
+      :rules="[rules.required(), rules.sentence()]"
+    />
 
-        <gen-button-group
-          v-model="form.serviceTypePay"
-          :label="$t('services.fields.service_type_by_payment_type')"
-        >
-          <v-btn value="Pospago">
-            {{ $t("services.fields.pospay") }}
-          </v-btn>
-          <v-btn class="ml-0" value="Prepago">
-            {{ $t("services.fields.prepay") }}
-          </v-btn>
-        </gen-button-group>
+    <gen-button-group
+      v-model="form.serviceTypePay"
+      :label="$t('services.fields.service_type_by_payment_type')"
+    >
+      <v-btn value="Pospago">
+        {{ $t("services.fields.pospay") }}
+      </v-btn>
+      <v-btn class="ml-0" value="Prepago">
+        {{ $t("services.fields.prepay") }}
+      </v-btn>
+    </gen-button-group>
 
-        <v-text-field
-          v-model="form.codcli"
-          :label="$t('services.fields.client_code')"
-          :rules="serviceCodeRules"
-        />
-        <v-switch
-          v-model="form.exclusivo"
-          :label="$t('services.fields.exclusive')"
-        />
-        <v-text-field v-model="form.crf" label="CRF" :rules="crfRules" />
+    <v-text-field
+      v-model="form.codcli"
+      :label="$t('services.fields.client_code')"
+      :rules="[rules.required()]"
+    />
+    <v-switch
+      v-model="form.exclusivo"
+      :label="$t('services.fields.exclusive')"
+    />
+    <v-text-field v-model="form.crf" label="CRF" :rules="[rules.required()]" />
 
-        <v-text-field
-          v-model.number="form.demanda"
-          :label="$t('services.fields.hired_demand') + ' (kW)'"
-          :rules="hiredDemandRules"
-        />
+    <v-text-field
+      v-model.number="form.demanda"
+      :label="$t('services.fields.hired_demand') + ' (kW)'"
+      :rules="[rules.required(), rules.number()]"
+    />
 
-        <v-col xs="12" cols="12" />
+    <v-col xs="12" cols="12" />
 
-        <v-autocomplete
-          v-model="form.tipo_tarifa"
-          :label="$t('services.fields.tarif_type')"
-          :items="tarifTypes"
-          hide-no-data
-          hide-details
-        />
+    <v-autocomplete
+      v-model="form.tipo_tarifa"
+      :label="$t('services.fields.tarif_type')"
+      :items="tarifTypes"
+      hide-no-data
+      hide-details
+    />
 
-        <gen-button-group
-          v-model="form.alimentacion"
-          :label="$t('services.fields.alimentation')"
-        >
-          <v-btn class="ml-0" value="Simple">
-            {{ $t("services.fields.alimentation_simple") }}
-          </v-btn>
-          <v-btn value="Doble">
-            {{ $t("services.fields.alimentation_double") }}
-          </v-btn>
-        </gen-button-group>
+    <gen-button-group
+      v-model="form.alimentacion"
+      :label="$t('services.fields.alimentation')"
+    >
+      <v-btn class="ml-0" value="Simple">
+        {{ $t("services.fields.alimentation_simple") }}
+      </v-btn>
+      <v-btn value="Doble">
+        {{ $t("services.fields.alimentation_double") }}
+      </v-btn>
+    </gen-button-group>
 
-        <gen-number-stepper
-          v-model="form.turnos"
-          show-number
-          :min="1"
-          :max="3"
-          :label="$t('services.fields.turns')"
-          :allow-number-editing="false"
-        />
+    <gen-number-stepper
+      v-model="form.turnos"
+      show-number
+      :min="1"
+      :max="3"
+      :label="$t('services.fields.turns')"
+      :allow-number-editing="false"
+    />
 
-        <v-col md="12" />
+    <v-col md="12" />
 
-        <gen-button-group
-          v-model="form.metraje"
-          :label="$t('services.fields.metraje')"
-        >
-          <v-btn class="ml-0" value="Alta"> Alta </v-btn>
-          <v-btn value="Baja"> Baja </v-btn>
-        </gen-button-group>
-        <v-text-field
-          v-model="capacityComputed"
-          :label="$t('services.fields.capacity') + ' (kVA)'"
-          :rules="capacityRules"
-          :disabled="disableCapacityInput"
-        />
+    <gen-button-group
+      v-model="form.metraje"
+      :label="$t('services.fields.metraje')"
+    >
+      <v-btn class="ml-0" value="Alta"> Alta </v-btn>
+      <v-btn value="Baja"> Baja </v-btn>
+    </gen-button-group>
+    <v-text-field
+      v-model="capacityComputed"
+      :label="$t('services.fields.capacity') + ' (kVA)'"
+      :rules="[rules.required(), rules.number()]"
+      :disabled="disableCapacityInput"
+    />
 
-        <v-autocomplete
-          v-if="form.metraje === 'Baja'"
-          v-model="serviceType"
-          label="Tipo de servicio (Segun transformación)"
-          :items="serviceTypes"
-          hide-no-data
-          hide-details
-        />
+    <v-autocomplete
+      v-if="form.metraje === 'Baja'"
+      v-model="serviceType"
+      label="Tipo de servicio (Segun transformación)"
+      :items="serviceTypes"
+      hide-no-data
+      hide-details
+    />
 
-        <div v-if="form.metraje === 'Baja'">
-          <v-subheader class="pl-0">
-            {{ $t("services.fields.transformers_amount") }}
-          </v-subheader>
+    <div v-if="form.metraje === 'Baja'">
+      <v-subheader class="pl-0">
+        {{ $t("services.fields.transformers_amount") }}
+      </v-subheader>
 
-          <v-btn-toggle group class="test">
-            <v-btn @click="transformerAmountMinusOne">
-              <v-icon>mdi-minus</v-icon>
-            </v-btn>
-
-            <v-btn @click="transformerAmountPlusOne">
-              <v-icon>mdi-plus</v-icon>
-            </v-btn>
-          </v-btn-toggle>
-        </div>
-
-        <v-col sm="12" md="12" class="pt-0 pb-0" />
-
-        <app-transformer-form
-          v-for="(transformer, i) in transformers"
-          :key="i"
-          :mono-phase="transformerCapacitiesMonoPhase.capacities"
-          :three-phase="transformerCapacitiesThreePhase.capacities"
-          v-model:type="transformer.type"
-          v-model:capacity="transformer.capacity"
-          @on-close-transformer="deleteTransformer(i)"
-        />
-
-        <v-btn
-          class="mx-0 font-weight-light btn-test"
-          color="primary"
-          large
-          @click="serviceCredentialsFormDialog = true"
-        >
-          {{ $t("services.fields.open_credentials_button") }}
+      <v-btn-toggle group class="test">
+        <v-btn @click="transformerAmountMinusOne">
+          <v-icon>mdi-minus</v-icon>
         </v-btn>
 
-        <v-dialog v-model="serviceCredentialsFormDialog" width="400">
-          <app-service-user-form
-            v-model:inserted-succesfully="insertedSuccesfully"
-            :accepted-ip="form.accepted_ip"
-            :device-token="form.device_token"
-            class-computed="ml-10 mr-10"
-            @on-delete-credentials="deleteServiceCredentials"
-            @click:submit="createServiceCredentials"
-            @click:cancel="closeServiceCredentialsFormDialog"
-          />
-        </v-dialog>
+        <v-btn @click="transformerAmountPlusOne">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-btn-toggle>
+    </div>
 
-        <slot name="form-actions" :serviceSubmit="createService">
-          <gen-form-actions
-            :loading-buttons="loading"
-            :enable-cancel="true"
-            @on-submit="createService"
-            @on-cancel="closeClick"
-          />
-        </slot>
-      </v-container>
-    </v-form>
+    <v-col sm="12" md="12" class="pt-0 pb-0" />
+
+    <app-transformer-form
+      v-for="(transformer, i) in transformers"
+      :key="i"
+      :mono-phase="transformerCapacitiesMonoPhase.capacities"
+      :three-phase="transformerCapacitiesThreePhase.capacities"
+      v-model:type="transformer.type"
+      v-model:capacity="transformer.capacity"
+      @on-close-transformer="deleteTransformer(i)"
+    />
+
+    <v-btn
+      class="mx-0 font-weight-light btn-test"
+      color="primary"
+      large
+      @click="serviceCredentialsFormDialog = true"
+    >
+      {{ $t("services.fields.open_credentials_button") }}
+    </v-btn>
+
+    <v-dialog v-model="serviceCredentialsFormDialog" width="400">
+      <app-service-user-form
+        v-model:inserted-succesfully="insertedSuccesfully"
+        :accepted-ip="form.accepted_ip"
+        :device-token="form.device_token"
+        class-computed="ml-10 mr-10"
+        @on-delete-credentials="deleteServiceCredentials"
+        @click:submit="createServiceCredentials"
+        @click:cancel="closeServiceCredentialsFormDialog"
+      />
+    </v-dialog>
   </gen-form>
 </template>
 
 <script>
-import { isNumber, isSentence } from "~/helpers/regex";
 import axios from "axios";
+
+import { required, number, sentence } from "@/utils/rules";
+
+import { deleteService, getService } from "@/services/app/service";
+
 import { mapActions } from "pinia";
-import { api } from "~/config";
 
 export default {
   name: "ServiceForm",
@@ -225,24 +215,11 @@ export default {
         capacities: [],
       },
       transformers: [],
-      serviceNameRules: [
-        (v) => !!v || "El nombre del servicio falta",
-        (v) => isSentence(v) || "Caracteres no permitidos",
-      ],
-      serviceCodeRules: [(v) => !!v || "El codigo del servicio falta"],
-      crfRules: [(v) => !!v || "El CRF falta"],
-      hiredDemandRules: [
-        (v) => !!v || "La demanda contratada falta",
-        (v) => isNumber(v) || "La Demanda contratada es un numero",
-      ],
-      meterNumberRules: [
-        (v) => !!v || "El numero del metro falta",
-        (v) => isNumber(v) || "Introdujo al menos un caracter incorrecto aqui",
-      ],
-      capacityRules: [
-        (v) => !!v || "La capacidad falta",
-        (v) => isNumber(v) || "La capacidad es un numero",
-      ],
+      rules: {
+        required,
+        number,
+        sentence,
+      },
     };
   },
   computed: {
@@ -315,9 +292,7 @@ export default {
       this.form.accepted_ip = null;
       this.$refs.form.resetValidation();
     },
-    reset() {
-      this.$refs.form.reset();
-    },
+
     cleanServiceCredentialsForm() {
       this.serviceUserForm.name = "";
       this.serviceUserForm.username = "";
@@ -325,19 +300,17 @@ export default {
       this.serviceUserForm.password_confirmation = "";
     },
     async deleteServiceCredentials() {
-      if (this.serviceId) {
-        const url = `${api.path("service")}/${this.serviceId}/user`;
+      if (!this.serviceId) return;
 
-        this.serviceId = null;
-        try {
-          await axios.delete(url);
-        } catch (error) {
-          return this.$emit("delete-unsuccesful");
-        }
-      } else {
+      try {
+        await deleteService(this.serviceId);
+      } catch (error) {
+        return this.$emit("delete-unsuccesful");
       }
+
       this.$emit("delete-succesful");
     },
+
     createServiceCredentials(credentials) {
       this.loading = true;
       this.form.device_token = credentials.device_token;
@@ -350,12 +323,15 @@ export default {
       this.loading = false;
       this.serviceCredentialsFormDialog = false;
     },
+
     deleteTransformer(i) {
       this.transformers.splice(i, 1);
     },
+
     closeServiceCredentialsFormDialog() {
       this.serviceCredentialsFormDialog = false;
     },
+
     async fillData() {
       await this.fillTransformersData();
       if (this.codcli) {
@@ -364,36 +340,33 @@ export default {
     },
 
     async fillForm() {
-      this.fetchService(this.codcli)
-        .then((res) => {
-          const transformers = res.data.transformers;
-          for (let i = 0; i < transformers.length; i++) {
-            this.transformers.push({
-              type: transformers[i].type,
-              capacity: transformers[i].capacity,
-            });
-          }
-          this.form = res.data;
-          this.setCredentialsFormData({
-            device_token: this.form.device_token,
-            accepted_ip: this.form.accepted_ip,
-            username: "",
-            password: "",
-            password_confirmation: "",
-          });
-          if (this.form.auth_id && this.codcli) {
-            this.serviceId = this.form.id;
-            this.setHasSensitiveData(true);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
+      const data = await getService(this.codcli);
+
+      const transformers = data.transformers;
+
+      for (let i = 0; i < transformers.length; i++) {
+        this.transformers.push({
+          type: transformers[i].type,
+          capacity: transformers[i].capacity,
         });
+      }
+
+      this.form = { ...data };
+
+      this.setCredentialsFormData({
+        device_token: this.form.device_token,
+        accepted_ip: this.form.accepted_ip,
+        username: "",
+        password: "",
+        password_confirmation: "",
+      });
+
+      if (this.form.auth_id && this.codcli) {
+        this.serviceId = this.form.id;
+        this.setHasSensitiveData(true);
+      }
     },
-    fetchService: function (id) {
-      const url = `${api.path("service")}/${id}`;
-      return axios.get(url);
-    },
+
     transformerAmountMinusOne() {
       if (this.transformers.length > 0) {
         this.transformers.pop();
@@ -425,9 +398,7 @@ export default {
 
       this.$emit("service-submit", this.form, serviceUserForm);
     },
-    closeClick() {
-      this.$emit("click:cancel");
-    },
+
     getTransformersId(transformers) {
       let tranformersIdsString = "";
       for (let i = 0; i < transformers.length; i++) {
@@ -437,6 +408,7 @@ export default {
       }
       return tranformersIdsString.slice(0, -1);
     },
+
     getTransformerId(transformer) {
       if (transformer.type === "mono") {
         const index = this.transformerCapacitiesMonoPhase.capacities.indexOf(
@@ -450,6 +422,7 @@ export default {
         return this.transformerCapacitiesThreePhase.ids[index];
       }
     },
+
     async fillTransformersData() {
       axios
         .get("/api/v1/transformer")
