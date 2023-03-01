c019e0bd62dca3f93ee946524541d30e600b9213
Jose Garcia <jogarcia@tuta.io>
2023-02-28 20:51:27 -0500

    Remove commented code

--- src/layouts/institutionContent.vue
+++ src/layouts/institutionContent.vue
@@ -8,72 +8,35 @@
 
 <script>
 export default {
-  name: 'InstitutionContent',
-  data () {
+  name: "InstitutionContent",
+  data() {
     return {
       links: [
         {
-          to: { name: 'institutions' },
-          icon: 'mdi-domain',
-          text: 'institutions.institution'
+          to: { name: "institutions" },
+          icon: "mdi-domain",
+          text: "institutions.institution",
         },
         {
-          to: { name: 'services' },
-          icon: 'mdi-view-dashboard',
-          text: 'services.service'
+          to: { name: "services" },
+          icon: "mdi-view-dashboard",
+          text: "services.service",
         },
         {
-          to: { name: 'read' },
-          icon: 'mdi-chart-bar-stacked ',
-          text: 'route.read'
+          to: { name: "read" },
+          icon: "mdi-chart-bar-stacked ",
+          text: "route.read",
         },
-        // {
-        //   to: '/plan-form',
-        //   icon: 'mdi-clipboard-outline',
-        //   text: 'route.plans'
-        // },
-        // {
-        //   to: '/consumption',
-        //   icon: 'mdi-chart-bar-stacked ',
-        //   text: 'route.consumption'
-        // },
-        // {
-        //   to: '/closure',
-        //   icon: 'mdi-chart-bar-stacked ',
-        //   text: 'route.closure'
-        // },
-        // {
-        //   to: '/user-management',
 
-        //   icon: 'mdi-account',
-        //   text: 'route.user-management'
-        // },
-        // {
-        //   to: '/monitorization',
-        //   icon: 'mdi-progress-clock ',
-        //   text: 'route.monitoring'
-        // },
-        // {
-        //   to: '/onure',
-        //   icon: 'mdi-home-analytics',
-        //   text: 'route.onure'
-        // },
-        // {
-        //   to: '/une',
-        //   icon: 'mdi-flash',
-        //   text: 'route.une'
-        // },
         {
-          to: { name: 'documents' },
-          icon: 'mdi-book-open-variant',
-          text: 'route.documents'
-        }
-      ]
-    }
-  }
-}
+          to: { name: "documents" },
+          icon: "mdi-book-open-variant",
+          text: "route.documents",
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
