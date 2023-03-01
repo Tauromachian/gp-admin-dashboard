865c8cf1101d824d119a7f0b2d05c9e74894f88b
Jose Garcia <jogarcia@tuta.io>
2023-02-28 20:46:04 -0500

    Update component

--- src/components/app/TransformerForm.vue
+++ src/components/app/TransformerForm.vue
@@ -1,11 +1,28 @@
 <template>
-  <div>
-    <v-btn-toggle group mandatory v-model="transformerType" color="primary">
-      <v-btn value="Monofásico">Monofásico</v-btn>
-      <v-btn value="Trifásico">Trifásico</v-btn>
+  <v-card elevation="0">
+    <v-btn-toggle v-model="typeComputed" group mandatory color="primary">
+      <v-btn value="mono"> Monofásico </v-btn>
+      <v-btn value="three"> Trifásico </v-btn>
     </v-btn-toggle>
-    <v-text-field label="Capacidad (kVA)" v-model="capacity" :rules="capacityRules" />
-  </div>
+    <v-btn
+      color="primary"
+      dark
+      small
+      absolute
+      top
+      right
+      fab
+      @click="closeTransformer"
+    >
+      <v-icon>mdi-cancel</v-icon>
+    </v-btn>
+    <v-autocomplete
+      v-model="capacityComputed"
+      :items="capacities"
+      label="Capacidad (kVA)"
+      :rules="capacityRules"
+    />
+  </v-card>
 </template>
 
 <script>
@@ -13,15 +30,67 @@ import { isNumber } from "~/helpers/regex";
 
 export default {
   name: "TransformerForm",
-  data: function() {
+  props: {
+    type: {
+      type: String,
+      default: "mono",
+    },
+    capacity: {
+      type: [Number, String],
+      default: "",
+    },
+    monoPhase: {
+      type: Array,
+      required: true,
+    },
+    threePhase: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: function () {
     return {
-      transformerType: "Monofásico",
-      capacity: "",
+      capacities: this.type === "mono" ? this.monoPhase : this.threePhase,
       capacityRules: [
-        v => !!v || "La capacidad falta",
-        v => isNumber(v) || "Introduzca un numero aqui"
-      ]
-    }
-  }
-}
+        (v) => !!v || "La capacidad falta",
+        (v) => isNumber(v) || "Introduzca un numero aqui",
+      ],
+    };
+  },
+  computed: {
+    capacityComputed: {
+      get() {
+        return this.capacity;
+      },
+      set(val) {
+        return this.$emit("update:capacity", val);
+      },
+    },
+    typeComputed: {
+      get() {
+        return this.type;
+      },
+      set(val) {
+        return this.$emit("update:type", val);
+      },
+    },
+  },
+  watch: {
+    type(val) {
+      this.capacities = this.getArrayByType(val);
+    },
+  },
+  methods: {
+    getArrayByType(transformerType) {
+      if (transformerType === "mono") {
+        return this.monoPhase;
+      } else {
+        return this.threePhase;
+      }
+    },
+    closeTransformer() {
+      this.$emit("on-close-transformer");
+    },
+  },
+};
 </script>
