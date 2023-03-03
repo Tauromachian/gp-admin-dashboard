fd6e6a86b7bb1c6c56d39d40c6f52e1568d48fc5
Jose Garcia <jogarcia@tuta.io>
2023-03-03 18:55:42 -0500

    Make stepper actions component

---
+++ src/components/generic/StepperActions.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="d-flex flex-row-reverse">
+    <v-btn
+      v-if="!isLastStep"
+      color="primary"
+      :disabled="disabled"
+      :loading="disabled"
+      @click="clickNext"
+    >
+      Siguiente
+    </v-btn>
+
+    <v-btn v-else @click="clickSubmit" :loading="disabled"> Submit </v-btn>
+
+    <v-btn
+      v-if="!isFirstStep"
+      :disabled="disabled"
+      color="primary"
+      variant="text"
+      @click="clickPrevious"
+      outlined
+      class="mr-2"
+      :loading="disabled"
+    >
+      Anterior
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StepperActionButtons",
+
+  props: {
+    isLastStep: {
+      type: Boolean,
+      default: false,
+    },
+    isFirstStep: {
+      type: Boolean,
+      default: false,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    clickNext() {
+      this.$emit("click:next");
+    },
+    clickPrevious() {
+      this.$emit("click:previous");
+    },
+    clickSubmit() {
+      this.$emit("click:submit");
+    },
+  },
+};
+</script>
+
+<style></style>
