bb01d6ab7f7c96e0c72e6f88276d54fb7fc51587
Jose Garcia <jogarcia@tuta.io>
2023-03-03 13:05:37 -0500

    Make BtnAdd component

---
+++ src/components/generic/BtnAdd.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-btn
+    class="mb-9 addition-button"
+    dark
+    color="primary"
+    size="x-large"
+    icon="mdi-plus"
+    @click="openInsertForm"
+    v-bind="$attrs"
+  >
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "BtnAdd",
+};
+</script>
+
+<style scoped>
+.addition-button {
+  position: fixed;
+  bottom: 1em;
+  right: 3em;
+}
+</style>
