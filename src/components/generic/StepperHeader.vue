e58f78a752f4ed1d5dc1897337d007058a0d75b1
Jose Garcia <jogarcia@tuta.io>
2023-03-03 18:56:05 -0500

    Make stepper sub components

---
+++ src/components/generic/StepperHeader.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="py-4">
+    <div class="w-100 d-flex justify-center mb-8">
+      <gen-stepper-step-header
+        class="d-flex justify-center"
+        :circle-content="step"
+        active
+        horizontal
+      >
+        {{ steps[step - 1] }}
+      </gen-stepper-step-header>
+    </div>
+    <div class="d-flex align-center">
+      <template v-for="(stepHeader, index) in steps" :key="`step-${index}`">
+        <gen-stepper-step-header
+          :active="step === index + 1"
+          :was-active="step > index + 1"
+          :circle-content="index + 1"
+          :small="true"
+        >
+          {{ stepHeader }}
+        </gen-stepper-step-header>
+        <v-divider
+          v-if="index !== steps.length - 1"
+          :thickness="2"
+          class="border-opacity-75"
+          :color="step > index + 1 ? 'primary' : 'grey'"
+        ></v-divider>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StepperHeader",
+
+  props: {
+    step: {
+      type: Number,
+      default: 0,
+    },
+    steps: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style scoped>
+.was-active {
+  color: #033562;
+  border-color: #033562;
+}
+
+.active {
+  background-color: #033562;
+  border-color: #033562;
+  color: white;
+}
+
+.active__text {
+  color: #033562;
+}
+</style>
---
+++ src/components/generic/StepperStepHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="align-center step-header" :class="{ flex: horizontal }">
+    <v-avatar :color="color">
+      <v-icon v-if="wasActive">mdi-check</v-icon>
+      <div v-else class="w-100 d-flex justify-center">
+        {{ circleContent }}
+      </div>
+    </v-avatar>
+    <div
+      v-if="!small"
+      :class="{
+        '-ml-10': !horizontal,
+        'mt-16': !horizontal,
+        absolute: !horizontal,
+        'ml-2': horizontal,
+      }"
+    >
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppStepperStepHeader",
+  props: {
+    active: {
+      type: Boolean,
+      default: false,
+    },
+    wasActive: {
+      type: Boolean,
+      default: false,
+    },
+    circleContent: {
+      type: [String, Number],
+      default: "",
+    },
+    horizontal: {
+      type: Boolean,
+      default: false,
+    },
+    small: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    color() {
+      if (this.active || this.wasActive) return "primary";
+
+      return "grey";
+    },
+  },
+};
+</script>
+
+<style scoped>
+.step-header {
+  position: relative;
+}
+</style>
