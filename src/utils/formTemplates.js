ce8faac8ea7f9ab15e07bc325aee20ba0ab2f78b
Jose Garcia <jogarcia@tuta.io>
2023-03-04 11:20:42 -0500

    Create formTemplates util

---
+++ src/utils/formTemplates.js
@@ -0,0 +1,24 @@
+export const SERVICE_FORM = {
+  name: "",
+  codcli: "",
+  serviceTypeTransformer: "",
+  serviceTypePay: "Pospago",
+  crf: "",
+  exclusivo: false,
+  tipo_tarifa: "M1A",
+  turnos: 1,
+  demanda: "",
+  meter_no: "",
+  alimentacion: "",
+  metraje: "",
+  capacidad: this.capacityComputed,
+  device_token: "",
+  accepted_ip: null,
+};
+
+export const CREDENTIALS_FORM = {
+  username: "",
+  name: "",
+  password: "",
+  password_confirmation: "",
+};
