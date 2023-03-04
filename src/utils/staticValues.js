4608cf5c048c6b3772550d4dd4cf6e43e600a9b3
Jose Garcia <jogarcia@tuta.io>
2023-03-04 10:51:37 -0500

    Make staticValues file

---
+++ src/utils/staticValues.js
@@ -0,0 +1,17 @@
+export const serviceTypes = [
+  "Monofásico 110 V",
+  "Monofásico 220 V",
+  "Trifásico 220 V",
+  "Trifásico 480 V",
+];
+
+export const tariffTypes = [
+  "A1",
+  "B1",
+  "M1A",
+  "M1C",
+  "M1D",
+  "M3A",
+  "M3B",
+  "M4",
+];
