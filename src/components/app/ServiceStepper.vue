66ed6daf16474889ed9c4baecf5da6347609416d
Jose Garcia <jogarcia@tuta.io>
2023-03-04 11:45:59 -0500

    Use correct attribute

--- src/components/app/ServiceStepper.vue
+++ src/components/app/ServiceStepper.vue
@@ -102,7 +102,7 @@ export default {
         this.step++;
       } catch (error) {
         this.addNotification({
-          type: "error",
+          color: "error",
           message: "Error al intentar continuar",
         });
       }
