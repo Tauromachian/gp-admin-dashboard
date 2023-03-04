aee8d23d6273b5ed0624b0d9504133dac83e06cb
Jose Garcia <jogarcia@tuta.io>
2023-03-04 11:16:28 -0500

    Make ServiceStepper component

---
+++ src/components/app/ServiceStepper.vue
@@ -0,0 +1,114 @@
+<template>
+  <v-card>
+    <v-card-title> Formulario de servicio </v-card-title>
+    <v-card-text>
+      <gen-stepper-header class="mb-8" :steps="steps" :step="step">
+      </gen-stepper-header>
+
+      <service-form v-model="form" v-if="step === 1"></service-form>
+
+      <service-metering-form v-model="form" v-if="step === 2">
+      </service-metering-form>
+
+      <service-user-form
+        v-if="step === 3"
+        v-model="formCredentials"
+        class-computed="ml-10 mr-10"
+        @on-delete-credentials="deleteServiceCredentials"
+        @click:submit="createServiceCredentials"
+        @click:cancel="closeServiceCredentialsFormDialog"
+      />
+    </v-card-text>
+
+    <v-card-text>
+      <gen-stepper-actions
+        :isFirstStep="step === 1"
+        :isLastStep="step === steps.length"
+        @click:next="confirmAndContinue"
+        @click:submit="confirmAndContinue"
+        @click:previous="step--"
+        :disabled="loading"
+      ></gen-stepper-actions>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import ServiceMeteringForm from "@/components/app/ServiceMeteringForm.vue";
+import ServiceForm from "@/components/app/ServiceForm.vue";
+import TransformerForm from "@/components/app/TransformerForm.vue";
+import ServiceUserForm from "@/components/app/ServiceUserForm.vue";
+
+export default {
+  name: "ServiceStepper",
+  components: {
+    TransformerForm,
+    ServiceForm,
+    ServiceUserForm,
+    ServiceMeteringForm,
+  },
+  props: {
+    modelValue: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      steps: ["Datos básicos", "Transformador", "Credenciales"],
+      step: 1,
+      loading: false,
+
+      form: {
+        name: "",
+        codcli: "",
+        serviceTypeTransformer: "",
+        serviceTypePay: "Pospago",
+        crf: "",
+        exclusivo: false,
+        tipo_tarifa: "M1A",
+        turnos: 1,
+        demanda: "",
+        meter_no: "",
+        alimentacion: "",
+        metraje: "",
+        capacidad: this.capacityComputed,
+        device_token: "",
+        accepted_ip: null,
+      },
+      formCredentials: {
+        username: "",
+        name: "",
+        password: "",
+        password_confirmation: "",
+      },
+
+      serviceCredentialsFormDialog: false,
+      serviceId: null,
+    };
+  },
+  methods: {
+    confirmAndContinue() {
+      this.step++;
+    },
+
+    async deleteServiceCredentials() {
+      if (!this.serviceId) return;
+
+      try {
+        await deleteService(this.serviceId);
+      } catch (error) {
+        return this.$emit("delete-unsuccesful");
+      }
+
+      this.$emit("delete-succesful");
+    },
+
+    async fillForm() {
+      throw new Error("Not implemented");
+    },
+  },
+};
+</script>
+
+<style></style>
