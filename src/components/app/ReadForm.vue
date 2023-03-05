c462064b1ed4981c432ef1c994db0e050ed36d51
Jose Garcia <jogarcia@tuta.io>
2023-03-05 16:31:21 -0500

    Remove redundant tag

--- src/components/app/ReadForm.vue
+++ src/components/app/ReadForm.vue
@@ -1,71 +1,69 @@
 <template>
-  <gen-form :title="$t('read.form_name')">
-    <v-form ref="form">
-      <v-container py-0>
-        <v-row wrap>
-          <v-col xs="12" sm="12" md="4" lg="4" xl="4" cols="12">
-            <v-text-field
-              v-model="form.T1IAE"
-              :label="$t('closure.fields.peak') + ' (kWh)'"
-              :rules="[rules.required(), rules.number()]"
-            />
-          </v-col>
-          <v-col xs="12" sm="12" md="4" lg="4" xl="4" cols="12">
-            <v-text-field
-              v-model="form.T2IAE"
-              :label="$t('closure.fields.day') + ' (kWh)'"
-              :rules="[rules.required(), rules.number()]"
-            />
-          </v-col>
-          <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
-            <v-text-field
-              v-model="form.T3IAE"
-              :label="$t('closure.fields.morning') + ' (kWh)'"
-              :rules="[rules.required(), rules.number()]"
-            />
-          </v-col>
-          <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
-            <v-text-field
-              v-model="form.TIRE"
-              :label="$t('closure.fields.reactive') + ' (kVAr)'"
-              :rules="[rules.required(), rules.number()]"
-            />
-          </v-col>
-          <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
-            <v-text-field
-              v-model="form.T1IMAXD"
-              label="MDP (kW)"
-              :rules="[rules.required(), rules.number()]"
-            />
-          </v-col>
-          <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
-            <v-text-field
-              v-model="form.T2IMAXD"
-              label="MDD (kW)"
-              :rules="[rules.required(), rules.number()]"
-            />
-          </v-col>
-          <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
-            <v-text-field
-              v-model="form.T3IMAXD"
-              label="MDM (kW)"
-              :rules="[rules.required(), rules.number()]"
-            />
-          </v-col>
-          <v-col cols="12">
-            <v-date-picker v-model="form.date" full-width color="primary" />
-          </v-col>
-          <slot name="form-actions" :closureSubmit="createClosure">
-            <gen-form-actions
-              :loading-buttons="loading"
-              :enable-cancel="true"
-              @on-submit="createClosure"
-              @on-cancel="onCloseClick"
-            />
-          </slot>
-        </v-row>
-      </v-container>
-    </v-form>
+  <gen-form ref="form" :title="$t('read.form_name')">
+    <v-container py-0>
+      <v-row wrap>
+        <v-col xs="12" sm="12" md="4" lg="4" xl="4" cols="12">
+          <v-text-field
+            v-model="form.T1IAE"
+            :label="$t('closure.fields.peak') + ' (kWh)'"
+            :rules="[rules.required(), rules.number()]"
+          />
+        </v-col>
+        <v-col xs="12" sm="12" md="4" lg="4" xl="4" cols="12">
+          <v-text-field
+            v-model="form.T2IAE"
+            :label="$t('closure.fields.day') + ' (kWh)'"
+            :rules="[rules.required(), rules.number()]"
+          />
+        </v-col>
+        <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
+          <v-text-field
+            v-model="form.T3IAE"
+            :label="$t('closure.fields.morning') + ' (kWh)'"
+            :rules="[rules.required(), rules.number()]"
+          />
+        </v-col>
+        <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
+          <v-text-field
+            v-model="form.TIRE"
+            :label="$t('closure.fields.reactive') + ' (kVAr)'"
+            :rules="[rules.required(), rules.number()]"
+          />
+        </v-col>
+        <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
+          <v-text-field
+            v-model="form.T1IMAXD"
+            label="MDP (kW)"
+            :rules="[rules.required(), rules.number()]"
+          />
+        </v-col>
+        <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
+          <v-text-field
+            v-model="form.T2IMAXD"
+            label="MDD (kW)"
+            :rules="[rules.required(), rules.number()]"
+          />
+        </v-col>
+        <v-col xs="12" sm="12" md="6" lg="6" xl="6" cols="12">
+          <v-text-field
+            v-model="form.T3IMAXD"
+            label="MDM (kW)"
+            :rules="[rules.required(), rules.number()]"
+          />
+        </v-col>
+        <v-col cols="12">
+          <v-date-picker v-model="form.date" full-width color="primary" />
+        </v-col>
+        <slot name="form-actions" :closureSubmit="createClosure">
+          <gen-form-actions
+            :loading-buttons="loading"
+            :enable-cancel="true"
+            @on-submit="createClosure"
+            @on-cancel="onCloseClick"
+          />
+        </slot>
+      </v-row>
+    </v-container>
   </gen-form>
 </template>
 
