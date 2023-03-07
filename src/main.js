4e40ef5f10c226bfa5733d5d76078fa4cd2fa3b3
Jose Garcia <jogarcia@tuta.io>
2023-03-07 15:53:19 -0500

    Import mirage in a way that won't be included in the bundle on development

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import App from "./App.vue";
 import { createApp } from "vue";
 import { registerPlugins } from "@/plugins";
-import { makeServer } from "./mock-server";
 
 const app = createApp(App);
 
 registerPlugins(app);
 
 if (process.env.NODE_ENV !== "production") {
+  const { makeServer } = await import(/* @vite-ignore */ "./mock-server");
   makeServer();
 }
 
