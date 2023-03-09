d293b5f8362b668e0086d931457734914af44517
Jose Garcia <jogarcia@tuta.io>
2023-03-09 11:03:19 -0500

    Make basic ReadFilter

--- src/components/app/ReadFilter.vue
+++ src/components/app/ReadFilter.vue
@@ -1,22 +1,28 @@
 <template>
-  <v-menu>
-    <template #activator="{ props }">
-      <v-btn
-        color="primary"
-        dark
-        fixed
-        fab
-        bottom
-        right
-        v-bind="props"
-        icon="mdi-filter"
+  <gen-filter>
+    <v-form>
+      <div
+        v-for="(field, index) in stepperFields"
+        :key="`${field.value}-${index}`"
       >
-      </v-btn>
-    </template>
-    <v-card>
-      <v-card-text> </v-card-text>
-    </v-card>
-  </v-menu>
+        <p>{{ field.text }}</p>
+        <v-range-slider
+          :min="1"
+          :max="10"
+          :step="1"
+          v-model="form[field.value]"
+        >
+        </v-range-slider>
+      </div>
+      <div class="d-flex flex-column">
+        <p>Rango de fecha</p>
+        <div class="d-flex">
+          <v-text-field class="mr-1" label="Fecha inicial"></v-text-field>
+          <v-text-field class="ml-1" label="Fecha final"></v-text-field>
+        </div>
+      </div>
+    </v-form>
+  </gen-filter>
 </template>
 
 <script>
@@ -28,13 +34,26 @@ export default {
       default: () => ({}),
     },
   },
+  data() {
+    return {
+      stepperFields: [
+        { text: "Pico (kWh)", value: "pico" },
+        { text: "Día (kWh)", value: "dia" },
+        { text: "Madrugada (kWh)", value: "madrugada" },
+        { text: "Reactivo (kVAr)", value: "reactivo" },
+        { text: "MDP (kWh)", value: "mdp" },
+        { text: "MDD (kWh)", value: "mdd" },
+        { text: "MDM (kWh)", value: "mdm" },
+      ],
+    };
+  },
   computed: {
-    selection: {
+    form: {
       get() {
         return this.modelValue;
       },
       set(val) {
-        this.$emit("input", val);
+        this.$emit("update:modelValue", val);
       },
     },
   },
