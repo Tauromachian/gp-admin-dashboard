c65d688317fb007fc22ead24d4f10663ead92ea4
Jose Garcia <jogarcia@tuta.io>
2023-03-09 16:22:19 -0500

    Make vue date picker wrapper

---
+++ src/components/generic/DatePicker.vue
@@ -0,0 +1,38 @@
+<template>
+  <vue-date-picker v-model="date" :enable-time-picker="false">
+    <template #trigger>
+      <v-text-field label="Fecha" v-model="date" readonly> </v-text-field>
+    </template>
+    <template #action-row="{ selectDate }">
+      <gen-form-actions @click:submit="selectDate"></gen-form-actions>
+    </template>
+  </vue-date-picker>
+</template>
+
+<script>
+import VueDatePicker from "@vuepic/vue-datepicker";
+import "@vuepic/vue-datepicker/dist/main.css";
+
+export default {
+  name: "DatePicker",
+  components: {
+    VueDatePicker,
+  },
+  props: {
+    modelValue: {
+      type: String,
+      default: "",
+    },
+  },
+  computed: {
+    date: {
+      get() {
+        return this.modelValue;
+      },
+      set(val) {
+        this.$emit("update:modelValue", val);
+      },
+    },
+  },
+};
+</script>
