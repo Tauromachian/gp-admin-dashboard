2a265bc99c5785c501b2971d9dd250fce8ea1f22
Jose Garcia <jogarcia@tuta.io>
2023-03-09 17:30:34 -0500

    Redo component

--- src/views/app/Plan.vue
+++ src/views/app/Plan.vue
@@ -1,61 +1,295 @@
 <template>
-  <v-container fill-height fluid grid-list-xl>
-    <v-row wrap>
-      <v-col md="12">
-        <v-card elevation="0">
-          <app-plan-one-year-table class="pt-0 pb-0" />
-          <v-btn
-            absolute
-            dark
-            fab
-            top
-            right
-            color="primary"
-            class="mt-6"
-            large
-            @click="formDialog = true"
-          >
-            <v-icon>mdi-plus</v-icon>
-          </v-btn>
-          <v-dialog v-model="formDialog" width="900">
-            <app-plan-one-year-form class-computed="ml-10 mr-10" @plan-submit="createPlan">
-              <template #form-actions="{ createPlan }">
-                <app-form-actions
-                  :loading-buttons="loading"
-                  :enable-cancel="true"
-                  @on-submit="createPlan"
-                  @on-cancel="closeUpdateDialog"
-                />
-              </template>
-            </app-plan-one-year-form>
-          </v-dialog>
-        </v-card>
-      </v-col>
-    </v-row>
+  <v-container fluid>
+    <v-card class="mt-4">
+      <v-card-text>
+        <gen-toolbar
+          v-model:visible-columns="visibleColumns"
+          :column-defs="headers"
+          @plus-click="openFormForInsert"
+          @edit-click="openFormForEdit"
+        >
+        </gen-toolbar>
+
+        <easy-data-table
+          class="mt-3"
+          v-model:items-selected="selectedRows"
+          :headers="visibleHeaders"
+          :items="plans"
+          :loading="loading"
+          single-select
+          show-select
+        >
+          <template #loading>
+            <v-progress-circular
+              color="primary"
+              :size="60"
+              :width="5"
+              indeterminate
+            ></v-progress-circular>
+          </template>
+        </easy-data-table>
+      </v-card-text>
+    </v-card>
+
+    <gen-btn-add @click="openFormForInsert"></gen-btn-add>
+
+    <v-dialog v-model="formDialog" width="400">
+      <plan-form
+        ref="form"
+        v-model="form"
+        :loading="loading"
+        @submit="submitPlan"
+      />
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
-export default {
-  name: 'Plan',
-  middleware: 'auth',
+import { mapActions, mapState } from "pinia";
+import { useNotificationsStore } from "@/stores/notifications";
 
-  data () {
+import { getPlans, addPlan, updatePlan } from "@/services/app/plan";
+
+import PlanForm from "@/components/app/PlanForm.vue";
+
+export default {
+  name: "Plan",
+  middleware: "auth",
+  components: {
+    PlanForm,
+  },
+  data() {
     return {
-      defaultColDef: null,
       formDialog: false,
-      loading: false
-    }
+      loading: false,
+      isFormUpdating: false,
+
+      selectedRows: [],
+      itemsPerPage: 10,
+      plans: [],
+
+      visibleColumns: [
+        "year",
+        "january",
+        "february",
+        "march",
+        "april",
+        "may",
+        "june",
+        "july",
+        "august",
+        "september",
+        "october",
+        "november",
+        "december",
+      ],
+
+      form: {
+        year: "",
+        january: "",
+        february: "",
+        march: "",
+        april: "",
+        may: "",
+        june: "",
+        july: "",
+        august: "",
+        september: "",
+        october: "",
+        november: "",
+        december: "",
+      },
+    };
+  },
+
+  computed: {
+    ...mapState("plan", ["serviceId", "services", "servicesImportantData"]),
+    tableData() {
+      return this.plans.map((row) => {
+        const plan = {
+          date: row.date,
+        };
+        return Object.assign(plan, row.planing);
+      });
+    },
+
+    headers() {
+      return [
+        {
+          text: `${this.$t("plans.year")}`,
+          value: "year",
+          checkboxSelection: true,
+        },
+        {
+          text: `${this.$t("plans.months.january")}`,
+          value: "january",
+        },
+        {
+          text: `${this.$t("plans.months.february")}`,
+          value: "february",
+        },
+        {
+          text: `${this.$t("plans.months.march")}`,
+          value: "march",
+        },
+        {
+          text: `${this.$t("plans.months.april")}`,
+          value: "april",
+        },
+        {
+          text: `${this.$t("plans.months.may")}`,
+          text: "MDD (kW)",
+          value: "may",
+        },
+        {
+          text: `${this.$t("plans.months.june")}`,
+          text: "MDM (kW)",
+          value: "june",
+        },
+        {
+          text: `${this.$t("plans.months.july")}`,
+          text: "MDM (kW)",
+          value: "july",
+        },
+        {
+          text: `${this.$t("plans.months.august")}`,
+          text: "MDM (kW)",
+          value: "august",
+        },
+        {
+          text: `${this.$t("plans.months.september")}`,
+          text: "MDM (kW)",
+          value: "september",
+        },
+        {
+          text: `${this.$t("plans.months.october")}`,
+          text: "MDM (kW)",
+          value: "october",
+        },
+        {
+          text: `${this.$t("plans.months.november")}`,
+          text: "MDM (kW)",
+          value: "november",
+        },
+        {
+          text: `${this.$t("plans.months.december")}`,
+          text: "MDM (kW)",
+          value: "december",
+        },
+        {
+          text: this.$t("plan.fields.date"),
+          value: "date",
+        },
+      ];
+    },
+    visibleHeaders() {
+      return this.headers.filter((header) =>
+        this.visibleColumns.includes(header.value)
+      );
+    },
+  },
+
+  created() {
+    this.loadData();
+  },
+
+  watch: {
+    filters() {},
   },
 
   methods: {
-    createPlan () {
+    ...mapActions(useNotificationsStore, ["addNotification"]),
 
+    async loadData() {
+      this.loading = true;
+      try {
+        this.plans = await getPlans();
+      } catch (error) {
+        this.addNotification({
+          type: "error",
+          message: error.message,
+        });
+      }
+      this.loading = false;
     },
 
-    closeUpdateDialog () {
-      this.formDialog = false
-    }
-  }
-}
+    openFormForInsert() {
+      if (this.isFormUpdating) {
+        this.$refs.form.reset();
+        this.isFormUpdating = false;
+      }
+      this.formDialog = true;
+    },
+
+    openFormForEdit() {
+      if (!this.isRowSelected("update")) return;
+
+      this.fillForm();
+      this.isFormUpdating = true;
+      this.formDialog = true;
+    },
+
+    isRowSelected(type) {
+      if (this.selectedRows.length !== 0) return true;
+
+      this.addNotification({
+        message: this.$t(
+          type === "delete"
+            ? "notifications.select_row_before_delete"
+            : "notifications.select_row_before_update"
+        ),
+        color: "info",
+      });
+
+      return false;
+    },
+
+    submitPlan() {
+      this.isFormUpdating ? this.updatePlan() : this.insertPlan();
+    },
+
+    async insertPlan() {
+      this.loading = true;
+
+      try {
+        await addPlan(this.form);
+        this.formDialog = false;
+        this.addNotification({
+          message: this.$t("notifications.succesfull_insert"),
+          color: "success",
+        });
+      } catch (err) {
+        this.addNotification({
+          message: this.$t("notifications.unsuccesfull_insert"),
+          color: "error",
+        });
+      }
+
+      this.loading = false;
+    },
+
+    async updatePlan() {
+      this.loading = true;
+      try {
+        await updatePlan(this.selectedRows[0].id, this.form);
+        this.formDialog = false;
+        this.addNotification({
+          message: this.$t("notifications.succesfull_insert"),
+          color: "success",
+        });
+      } catch (err) {
+        this.addNotification({
+          message: this.$t("notifications.unsuccesfull_insert"),
+          color: "error",
+        });
+      }
+
+      this.loading = false;
+    },
+
+    fillForm() {
+      this.form = { ...this.selectedRows[0] };
+    },
+  },
+};
 </script>
