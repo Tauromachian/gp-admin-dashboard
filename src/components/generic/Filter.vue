ea5bec155517147f14e6b2f09eed1dd97d1c3019
Jose Garcia <jogarcia@tuta.io>
2023-03-09 09:06:04 -0500

    Simplify component

--- src/components/generic/Filter.vue
+++ src/components/generic/Filter.vue
@@ -1,35 +1,17 @@
 <template>
   <v-menu
     :close-on-content-click="false"
-    bottom
-    left
     min-width="300"
     max-width="300"
     nudge-left="12"
     offset-x
-    transition="slide-y-transition"
     v-model="menu"
   >
     <template #activator="{ props }">
-      <v-btn
-        class="elevation-0"
-        :class="{ 'position-right': right, 'position-left': left }"
-        color="grey"
-        dark
-        fab
-        fixed
-        style="top: 96px"
-        top
-        left
-        v-bind="props"
-      >
-        <v-icon>mdi-filter</v-icon>
-      </v-btn>
+      <v-btn color="primary" dark v-bind="props" icon="mdi-filter"> </v-btn>
     </template>
     <v-card>
-      <v-container grid-list-xl>
-        <slot />
-      </v-container>
+      <slot />
     </v-card>
   </v-menu>
 </template>
@@ -42,29 +24,5 @@ export default {
       menu: false,
     };
   },
-  props: {
-    left: {
-      type: Boolean,
-      default: false,
-    },
-    right: {
-      type: Boolean,
-      default: false,
-    },
-  },
 };
 </script>
-
-<style scoped>
-.position-right {
-  border-bottom-right-radius: 0;
-  border-top-right-radius: 0;
-  right: 0;
-}
-
-.position-left {
-  border-bottom-left-radius: 0;
-  border-top-left-radius: 0;
-  left: 0;
-}
-</style>
