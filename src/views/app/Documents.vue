2cf9053f646e32aa16d13ebeeefd7fcebc92704e
Jose Garcia <jogarcia@tuta.io>
2023-03-10 16:09:14 -0500

    Move info to data props

--- src/views/app/Documents.vue
+++ src/views/app/Documents.vue
@@ -1,131 +1,30 @@
 <template>
   <v-container class="container">
     <v-row>
-      <v-col lg="4" md="6" sm="6" xs="12">
+      <v-col
+        lg="4"
+        md="6"
+        sm="6"
+        xs="6"
+        v-for="(document, index) in documents"
+        :key="`document-${index}`"
+      >
         <v-card>
-          <v-card-title>Ley Eléctrica 1287 de 1975</v-card-title>
-          <v-card-text
-            >Ley que regula el uso de la energía en Cuba.</v-card-text
-          >
+          <v-card-title>{{ document.title }} </v-card-title>
+          <v-card-text>{{ document.description }}</v-card-text>
           <v-card-actions>
-            <v-btn text color="primary" @click="openPdf('./LeyElectrica.pdf')">
-              {{ $t("button.actions.see") }}
-            </v-btn>
-            <v-btn text download href="./LeyElectrica.pdf" color="primary">
-              {{ $t("button.actions.download") }}
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col lg="4" md="6" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Res-277-2014 Tarifas no residenciales</v-card-title>
-          <v-card-text
-            >Resolución para el control de las tarifas energéticas no
-            residenciales.</v-card-text
-          >
-          <v-card-actions>
+            <v-spacer></v-spacer>
             <v-btn
-              text
+              variant="text"
               color="primary"
-              @click="openPdf('./02 Res-277-2014 Tarifas no residencial.pdf')"
+              @click="openPdf(document.fileSource)"
             >
               {{ $t("button.actions.see") }}
             </v-btn>
             <v-btn
-              text
+              variant="text"
               download
-              href="./02 Res-277-2014 Tarifas no residencial.pdf"
-              color="primary"
-            >
-              {{ $t("button.actions.download") }}
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col lg="4" md="6" sm="6" xs="12">
-        <v-card>
-          <v-card-title
-            >Res-152-2018 Manual de Inspección de Portadores
-            Energéticos</v-card-title
-          >
-          <v-card-text
-            >Manual para la realización de las inspecciones a los portadores
-            energéticos.</v-card-text
-          >
-          <v-card-actions>
-            <v-btn
-              text
-              color="primary"
-              @click="
-                openPdf(
-                  './03 Res-152-2018 Manual de Insp Portadores Energeticos.pdf'
-                )
-              "
-            >
-              {{ $t("button.actions.see") }}
-            </v-btn>
-            <v-btn
-              text
-              download
-              href="./03 Res-152-2018 Manual de Insp Portadores Energeticos.pdf"
-              color="primary"
-            >
-              {{ $t("button.actions.download") }}
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col lg="4" md="6" sm="6" xs="12">
-        <v-card>
-          <v-card-title>Metodología acomodo de carga</v-card-title>
-          <v-card-text>
-            Metodología para la confección de los estudios de control,
-            regulación y acomodo de la carga energética.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn
-              text
-              color="primary"
-              @click="openPdf('./04 Metodologia acomodo de carga.pdf')"
-            >
-              {{ $t("button.actions.see") }}
-            </v-btn>
-            <v-btn
-              text
-              download
-              href="./04 Metodologia acomodo de carga.pdf"
-              color="primary"
-            >
-              {{ $t("button.actions.download") }}
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-      <v-col lg="4" md="6" sm="6" xs="12">
-        <v-card>
-          <v-card-title
-            >Dec-Ley 345-2019 Desarrollo energías renovables</v-card-title
-          >
-          <v-card-text
-            >Ley para el desarrollo de energías renovables.</v-card-text
-          >
-          <v-card-actions>
-            <v-btn
-              text
-              color="primary"
-              @click="
-                openPdf(
-                  './05 Dec-Ley 345-2019 Desarrollo energias renovables.pdf'
-                )
-              "
-            >
-              {{ $t("button.actions.see") }}
-            </v-btn>
-            <v-btn
-              text
-              download
-              href="./05 Dec-Ley 345-2019 Desarrollo energias renovables.pdf"
+              :href="document.fileSource"
               color="primary"
             >
               {{ $t("button.actions.download") }}
@@ -140,6 +39,42 @@
 <script>
 export default {
   name: "Documents",
+  data() {
+    return {
+      documents: [
+        {
+          title: "Ley Eléctrica 1287 de 1975",
+          description: "Ley que regula el uso de la energía en Cuba.",
+          fileSource: "./LeyElectrica.pdf",
+        },
+        {
+          title: "Res-277-2014 Tarifas no residenciales",
+          description:
+            "Resolución para el control de las tarifas energéticas no residenciales.",
+          fileSource: "./02 Res-277-2014 Tarifas no residencial.pdf",
+        },
+        {
+          title: "Res-152-2018 Manual de Inspección de Portadores Energéticos",
+          description:
+            "Manual para la realización de las inspecciones a los portadores energéticos.",
+          fileSource:
+            "./03 Res-152-2018 Manual de Insp Portadores Energeticos.pdf",
+        },
+        {
+          title: "Metodología acomodo de carga",
+          description:
+            "Metodología para la confección de los estudios de control, regulación y acomodo de la carga energética.",
+          fileSource: "./04 Metodologia acomodo de carga.pdf",
+        },
+        {
+          title: "Dec-Ley 345-2019 Desarrollo energías renovables",
+          description: "Ley para el desarrollo de energías renovables.",
+          fileSource:
+            "./05 Dec-Ley 345-2019 Desarrollo energias renovables.pdf",
+        },
+      ],
+    };
+  },
   methods: {
     openPdf(url) {
       window.open(url);
