9a51e3b11911f26ac82b019fff1781cb4873abb6
Jose Garcia <jogarcia@tuta.io>
2023-03-10 10:36:07 -0500

    Add optional indicator

--- src/components/app/PlanForm.vue
+++ src/components/app/PlanForm.vue
@@ -15,73 +15,73 @@
     <v-text-field
       v-model="form.february"
       :rules="[rules.number()]"
-      label="Enero (kWh)"
+      label="Enero (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.march"
       :rules="[rules.number()]"
-      label="Febrero (kWh)"
+      label="Febrero (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.april"
       :rules="[rules.number()]"
-      label="Marzo (kWh)"
+      label="Marzo (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.may"
       :rules="[rules.number()]"
-      label="Abril (kWh)"
+      label="Abril (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.june"
       :rules="[rules.number()]"
-      label="Mayo (kWh)"
+      label="Mayo (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.july"
       :rules="[rules.number()]"
-      label="Junio (kWh)"
+      label="Junio (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.august"
       :rules="[rules.number()]"
-      label="Julio (kWh)"
+      label="Julio (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.september"
       :rules="[rules.number()]"
-      label="Agosto (kWh)"
+      label="Agosto (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.january"
       :rules="[rules.number()]"
-      label="Septiembre (kWh)"
+      label="Septiembre (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.october"
       :rules="[rules.number()]"
-      label="Octubre (kWh)"
+      label="Octubre (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.november"
       :rules="[rules.number()]"
-      label="Noviembre (kWh)"
+      label="Noviembre (kWh) (opcional)"
     />
 
     <v-text-field
       v-model="form.december"
       :rules="[rules.number()]"
-      label="Diciembre (kWh)"
+      label="Diciembre (kWh) (opcional)"
     />
   </gen-form>
 </template>
