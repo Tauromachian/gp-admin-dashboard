b3376392adb3dfc66e18f4e2b049812701b6106d
Jose Garcia <jogarcia@tuta.io>
2023-03-13 09:55:56 -0400

    Display user name

--- src/components/generic/AccountButtonMenu.vue
+++ src/components/generic/AccountButtonMenu.vue
@@ -6,14 +6,19 @@
       </v-btn>
     </template>
     <v-list>
+      <v-list-item>
+        <template #prepend>
+          <v-icon icon="mdi-account"></v-icon>
+        </template>
+        <v-list-item-title>{{ user.name }}</v-list-item-title>
+      </v-list-item>
+      <v-divider />
       <v-list-item @click="() => $emit('logout')">
         <template #prepend>
           <v-icon icon="mdi-logout-variant"></v-icon>
         </template>
         <v-list-item-title>{{ $t("button.user.logout") }}</v-list-item-title>
       </v-list-item>
-
-      <v-divider />
     </v-list>
   </v-menu>
 </template>
@@ -21,5 +26,11 @@
 <script>
 export default {
   name: "AccountButtonMenu",
+  props: {
+    user: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
 };
 </script>
