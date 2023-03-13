d3da749f9157a477a4bdaaf43dfc1b8633198c5c
Jose Garcia <jogarcia@tuta.io>
2023-03-13 09:54:53 -0400

    Use pinia persist

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,9 +2,11 @@
 import { createPinia } from "pinia";
 import { markRaw } from "vue";
 import router from "@/router";
+import piniaPersist from "pinia-plugin-persist";
 
 const pinia = createPinia();
 
+pinia.use(piniaPersist);
 pinia.use(({ store }) => (store.router = markRaw(router)));
 
 export default pinia;
